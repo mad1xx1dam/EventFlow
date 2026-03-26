c763624a25dea8720245ab35a5cea2ffbef11675
mad1xxx <gorobets.maks@bk.ru>
2026-03-26 21:40:58 +1000

    fixed logging

--- backend/src/main/java/com/eventflow/eventflow_backend/config/RequestLoggingConfig.java
+++ backend/src/main/java/com/eventflow/eventflow_backend/config/RequestLoggingConfig.java
@@ -10,13 +10,8 @@ public class RequestLoggingConfig {
     @Bean
     public CommonsRequestLoggingFilter logFilter() {
         CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
-        // включаем логирование строки запроса (?param=value)
-        filter.setIncludeQueryString(true);
-        // включаем логирование тела запроса (payload)
-        filter.setIncludePayload(true);
-        // ограничиваем длину тела, чтобы логи не лопнули от больших файлов
-        filter.setMaxPayloadLength(10000);
-        // можно включить хедеры (true), но обычно они дают слишком много мусора
+        filter.setIncludeQueryString(false);
+        filter.setIncludePayload(false);
         filter.setIncludeHeaders(false);
         filter.setIncludeClientInfo(true);
         return filter;
